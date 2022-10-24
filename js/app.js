13921d86bf28ed6c56f72d110984bb141d1c3683
davidcalero <calerodonaire@gmail.com>
2022-10-24 20:39:43 +0200

    day 1 of DOM

---
+++ js/app.js
@@ -0,0 +1,8 @@
+let nav = document.querySelector("nav");
+console.log(nav);
+
+let active = document.querySelector(".active");
+console.log(active);
+
+let headersAndParagraphs = document.querySelectorAll("p ,h1");
+console.log(headersAndParagraphs[0].innerText);
