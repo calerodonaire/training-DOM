eccf5490147481829f86073da2c8d7550ae7d169
davidcalero <calerodonaire@gmail.com>
2022-10-27 17:31:04 +0200

    events

--- js/app.js
+++ js/app.js
@@ -1,17 +1,40 @@
-document.body.addEventListener("click", () => {
-  alert("this is the body");
-});
-document.querySelector("section").addEventListener("click", () => {
-  alert("this is the section");
-});
-document.querySelector("div").addEventListener("click", () => {
-  alert("this is the div");
-});
-document.addEventListener("keydown", (event) => {
+// document.body.addEventListener("click", () => {
+//   alert("this is the body");
+// });
+// document.querySelector("section").addEventListener("click", () => {
+//   alert("this is the section");
+// });
+// document.querySelector("div").addEventListener("click", () => {
+//   alert("this is the div");
+// });
+// document.addEventListener("keydown", (event) => {
+//   event.preventDefault();
+//   console.log(event.key);
+//   console.log(event.code);
+//   if (event.key == "Enter") {
+//     alert("hello world!");
+//   }
+// });
+
+// let time1 = 0.0,
+//   time2 = 0.0;
+// document.body.addEventListener("click", (event) => {
+//   console.log(event);
+//   document.body.style.backgroundColor = "black";
+//   time1 = event.timeStamp;
+//   console.log(time1);
+//   console.log(event.currentTarget);
+// });
+// document.body.addEventListener("click", (event) => {
+//   setTimeout(() => {
+//     alert("hello world!");
+//     time2 = event.timeStamp;
+//     console.log(time2);
+//     console.log(time1 - time2);
+//   }, 3000);
+// });
+
+console.log(document.forms);
+document.forms[0].onsubmit = (event) => {
   event.preventDefault();
-  console.log(event.key);
-  console.log(event.code);
-  if (event.key == "Enter") {
-    alert("hello world!");
-  }
-});
+};
