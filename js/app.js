45dc544de234af4d0040ad002cdce54f416a0d1e
davidcalero <calerodonaire@gmail.com>
2022-11-02 21:33:56 +0100

    a falta de las mejoras de la práctica

--- js/app.js
+++ js/app.js
@@ -10,12 +10,14 @@ window.addEventListener("load", () => {
   let trash = document.querySelectorAll(".fa-trash");
   let edit = document.querySelectorAll(".fa-pencil");
   let task = document.querySelectorAll(".task");
+
   // console.log(input);
   // Events
   // Close the alert
   closeBttn.addEventListener("click", () => {
     alert.classList.toggle("dismissible");
   });
+  //sirve para que si el usuario hace enter, no haga nada.
   input.addEventListener("focus", () => {
     document.addEventListener("keydown", (e) => {
       if (e.code == "Enter" || e.code == "NumpadEnter") {
@@ -24,10 +26,12 @@ window.addEventListener("load", () => {
     });
   });
   arrow.addEventListener("click", (e) => {
-    // ELiminar los espacios al principio y al final del string.
+    //que no se pueda enviar el input vacío, trim sirve para eliminar espacios al principio/final del string
     if (input.value.trim() == "") {
-      console.log("Empty");
+      // console.log("Empty");
+      //que no se pueda enviar vacío
       e.preventDefault(); // Prevent submit
+      //que vuelva a poner el input vacío
       input.value = ""; // Reset value
       alert.classList.remove("dismissible");
     } else {
@@ -35,32 +39,36 @@ window.addEventListener("load", () => {
       input.value = "";
       id =
         Number(document.querySelector("tbody")?.lastElementChild?.id) + 1 || 0;
-
       document.querySelector("tbody").appendChild(generateRow(id, text));
       if (!alert.classList.contains("dismissible")) {
         alert.classList.add("dismissible");
       }
     }
   });
-  // done.addEventListener("click", (e) =>{
-  //   console.log("working");
-  // })
+  // console.log(done);
+  // done[1].element.addEventListener('click', (e) => {console.log("working");});
+
+  //done item
   done.forEach((item) => {
     item.addEventListener("click", (e) => {
       deleteTask(e);
     });
   });
+
+  //trash item
   trash.forEach((item) => {
     item.addEventListener("click", (e) => {
-      // deleteTask(e);
-      removeRow(e);
+      removeRow(e, false);
     });
   });
+
+  //edit item
   edit.forEach((item) => {
     item.addEventListener("click", (e) => {
       editTask(e, false);
     });
   });
+
   task.forEach((item) => {
     item.addEventListener("focus", (e) => {
       editTask(e, true);
@@ -68,77 +76,122 @@ window.addEventListener("load", () => {
   });
 });
 
-let editTask = (e, onFocus) => {
+// Functions
+const editTask = (e, onFocus) => {
+  let editable = e;
   if (onFocus) {
-    console.log(e.target);
-    e.target.classList.add("editable");
+    // console.log(e.target);
+    editable.target.classList.add("editable");
+    document.addEventListener("keydown", (e) => {
+      console.log(e.code);
+      if (e.code == "Escape") {
+        editable.target.classList.remove("editable");
+        editable.target.blur();
+        if (editable.target.innerHTML == "") {
+          removeRow(editable, true);
+        }
+      }
+    });
+    editable.target.addEventListener("blur", () => {
+      editable.target.classList.remove("editable");
+      if (editable.target.innerHTML == "") {
+        removeRow(editable, true);
+      }
+    });
   } else {
     let editable =
-      e.targt.parentNode.parentNode.previousElementSibling.lastElementChild;
-
+      e.target.parentNode.parentNode.previousElementSibling.lastElementChild;
+    // console.log(editable);
     editable.classList.add("editable");
     editable.focus();
   }
 };
 
-let deleteTask = (e) => {
+const deleteTask = (e) => {
   let task = e.target.nextElementSibling;
   text = task.innerHTML;
 
   if (text.includes("<del>")) {
     task.innerHTML = task.firstElementChild.textContent;
-    task.setAttribute("data-complete", "false");
+    task.setAttribute("data-completed", "false");
   } else {
     task.innerHTML = `<del>${text}</del>`;
-    task.setAttribute("data-complete", "true");
+    task.setAttribute("data-completed", "true");
   }
 };
 
-let removeRow = (e) => {
-  e.target.parentNode.parentNode.parentNode.remove();
+const removeRow = (e, editing) => {
+  if (editing) {
+    e.target.parentNode.parentNode.remove();
+  } else {
+    e.target.parentNode.parentNode.parentNode.remove();
+  }
 };
 
-let generateRow = (id, text) => {
+//Refactorizamos el códogo encapsulando la función
+const generateRow = (id, text) => {
   // Creando una nueva fila.
   let newRow = document.createElement("tr");
   newRow.setAttribute("id", id);
   newRow.innerHTML = `
-     <td>
-     <i class="fa-solid fa-circle-check fa-2x"></i>
-     <span
-     class="task"
-     contenteditable="true">
-     ${text}
-     </span>
-     </td>
-     <td>
-     <span class="fa-stack fa-2x">
-     <i class="fa-solid fa-square fa-stack-2x"></i>
-     <i class="fa-solid fa-pencil fa-stack-1x fa-inverse"></i>
-     </span>
-     </td>
-     <td>
-     <span class="fa-stack fa-2x">
-     <i class="fa-solid fa-square fa-stack-2x"></i>
-     <i class="fa-solid fa-trash fa-stack-1x fa-inverse"></i>
-     </span>
-     </td>
-     `;
+      <td>
+      <i class="fa-solid fa-circle-check fa-2x"></i>
+      <span
+      class="task"
+      contenteditable="true">
+      ${text}
+      </span>
+      </td>
+      <td>
+      <span class="fa-stack fa-2x">
+      <i class="fa-solid fa-square fa-stack-2x"></i>
+      <i class="fa-solid fa-pencil fa-stack-1x fa-inverse"></i>
+      </span>
+      </td>
+      <td>
+      <span class="fa-stack fa-2x">
+      <i class="fa-solid fa-square fa-stack-2x"></i>
+      <i class="fa-solid fa-trash fa-stack-1x fa-inverse"></i>
+      </span>
+      </td>
+      `;
+
+  newRow.firstElementChild.firstElementChild.addEventListener("click", (e) => {
+    deleteTask(e);
+  });
+  newRow.firstElementChild.lastElementChild.addEventListener("click", (e) => {
+    editTask(e, true);
+  });
+  newRow.firstElementChild.nextElementSibling.firstElementChild.addEventListener(
+    "click",
+    (e) => {
+      editTask(e, false);
+    }
+  );
+  newRow.lastElementChild.firstElementChild.addEventListener("click", (e) => {
+    removeRow(e, false);
+  });
+
   return newRow;
 };
-
 // window.addEventListener("load", () => {
-//   let id = 0; // id of the current task
-//   let text = ""; // text of the current task
-//   let alert = document.querySelector(".alert"); // alert element
-//   let close = alert.firstElementChild; // button element in the alert element
-//   let input = document.querySelector("#task"); // input element
-//   let arrow = document.querySelector(".arrow"); // arrow element
+//   // Variables
+//   let id = 0;
+//   let text = "";
+//   let alert = document.querySelector(".alert");
+//   let closeBttn = alert.firstElementChild;
+//   let input = document.querySelector("#task");
+//   let arrow = document.querySelector(".arrow");
 //   let done = document.querySelectorAll(".fa-circle-check");
 //   let trash = document.querySelectorAll(".fa-trash");
 //   let edit = document.querySelectorAll(".fa-pencil");
 //   let task = document.querySelectorAll(".task");
-
+//   // console.log(input);
+//   // Events
+//   // Close the alert
+//   closeBttn.addEventListener("click", () => {
+//     alert.classList.toggle("dismissible");
+//   });
 //   input.addEventListener("focus", () => {
 //     document.addEventListener("keydown", (e) => {
 //       if (e.code == "Enter" || e.code == "NumpadEnter") {
@@ -146,108 +199,143 @@ let generateRow = (id, text) => {
 //       }
 //     });
 //   });
-
-//   input.addEventListener("blur", () => {
-//     input.setAttribute("placeholder", "Add a new task");
-//   });
-
 //   arrow.addEventListener("click", (e) => {
+//     // ELiminar los espacios al principio y al final del string.
 //     if (input.value.trim() == "") {
-//       //trim removes the white spaces
-//       e.preventDefault();
+//       console.log("Empty");
+//       e.preventDefault(); // Prevent submit
+//       input.value = ""; // Reset value
 //       alert.classList.remove("dismissible");
-//       input.value = "";
 //     } else {
-//       text = input.value;
+//       let text = input.value;
 //       input.value = "";
 //       id =
 //         Number(document.querySelector("tbody")?.lastElementChild?.id) + 1 || 0;
+
 //       document.querySelector("tbody").appendChild(generateRow(id, text));
 //       if (!alert.classList.contains("dismissible")) {
 //         alert.classList.add("dismissible");
 //       }
 //     }
 //   });
-
-//   // done.addEventListener('click', (e) => {
+//   // done.addEventListener("click", (e) =>{
 //   //   console.log("working");
-//   // });
-
+//   // })
 //   done.forEach((item) => {
 //     item.addEventListener("click", (e) => {
 //       deleteTask(e);
 //     });
 //   });
-
 //   trash.forEach((item) => {
 //     item.addEventListener("click", (e) => {
-//       removeRow(e);
+//       // deleteTask(e);
+//       removeRow(e, false);
 //     });
 //   });
-
 //   edit.forEach((item) => {
 //     item.addEventListener("click", (e) => {
 //       editTask(e, false);
 //     });
 //   });
-
 //   task.forEach((item) => {
-//     item.addEventListener("focus",(e) => {
+//     item.addEventListener("focus", (e) => {
 //       editTask(e, true);
 //     });
 //   });
-
 // });
 
-// let editTask = (e, onFocus) => {
+// //Functions
+// const editTask = (e, onFocus) => {
+//   let editable = e;
+//   if (onFocus) {
+//     // console.log(e.target);
+//     e.target.classList.add("editable");
+//     document.addEventListener("keydown", (e) => {
+//       // console.log(e.code);
+//       if (e.code == "Escape") {
+//         editable.target.classList.remove("editable");
+//         editable.target.blur();
+//         if (editable.target.innerHTML == "") {
+//           removeRow(editable, true);
+//         }
+//       }
+//     });
+//     editable.target.addEventListener("blur", () => {
+//       editable.target.classList.remove("editable");
+//       if (editable.target.innerHTML == "") {
+//         removeRow(editable, true);
+//       }
+//     });
+//   } else {
+//     let editable =
+//       e.targt.parentNode.parentNode.previousElementSibling.lastElementChild;
 
-//   if(onFocus){
-//     console.log(e.target);
-
-//   }else{
-//     let editable = e.target.parentNode.parentNode.previousElementSibling.lastElementChild;
-//     console.log(editable);
 //     editable.classList.add("editable");
 //     editable.focus();
 //   }
-// }
+// };
 
-// let deleteTask = (e) => {
+// const deleteTask = (e) => {
 //   let task = e.target.nextElementSibling;
 //   text = task.innerHTML;
 
 //   if (text.includes("<del>")) {
 //     task.innerHTML = task.firstElementChild.textContent;
-//     task.setAttribute("data-completed", "false");
+//     task.setAttribute("data-complete", "false");
 //   } else {
 //     task.innerHTML = `<del>${text}</del>`;
-//     task.setAttribute("data-completed", "true");
+//     task.setAttribute("data-complete", "true");
 //   }
 // };
 
-// let removeRow = (e) => {
-//   e.target.parentNode.parentNode.parentNode.remove();
+// const removeRow = (e, editing) => {
+//   if (editing) {
+//     e.target.parentNode.parentNode.remove();
+//   } else {
+//     e.target.parentNode.parentNode.parentNode.remove();
+//   }
 // };
 
-// let generateRow = (id, text) => {
+// const generateRow = (id, text) => {
+//   // Creando una nueva fila.
 //   let newRow = document.createElement("tr");
 //   newRow.setAttribute("id", id);
 //   newRow.innerHTML = `
-//     <td>
-//       <i class="fa-solid fa-circle-check"></i>
-//       <span class="task" contenteditable="true">${text}</span>
-//     <td>
-//       <span class="fa-stack fa-2x">
-//         <i class="fa-solid fa-square fa-stack-2x"></i>
-//         <i class="fa-solid fa-pencil fa-stack-1x fa-inverse"></i>
-//       </span>
-//     </td>
-//     <td>
-//       <span class="fa-stack fa-2x">
-//         <i class="fa-solid fa-square fa-stack-2x"></i>
-//         <i class="fa-solid fa-trash fa-stack-1x fa-inverse"></i>
-//       </span>
-//     </td>`;
-
+//      <td>
+//      <i class="fa-solid fa-circle-check fa-2x"></i>
+//      <span
+//      class="task"
+//      contenteditable="true">
+//      ${text}
+//      </span>
+//      </td>
+//      <td>
+//      <span class="fa-stack fa-2x">
+//      <i class="fa-solid fa-square fa-stack-2x"></i>
+//      <i class="fa-solid fa-pencil fa-stack-1x fa-inverse"></i>
+//      </span>
+//      </td>
+//      <td>
+//      <span class="fa-stack fa-2x">
+//      <i class="fa-solid fa-square fa-stack-2x"></i>
+//      <i class="fa-solid fa-trash fa-stack-1x fa-inverse"></i>
+//      </span>
+//      </td>
+//      `;
+//   newRow.firstElementChild.firstElementChild.addEventListener("click", (e) => {
+//     deleteTask(e);
+//   });
+//   newRow.firstElementChild.lastElementChild.addEventListener("click", (e) => {
+//     editTask(e, true);
+//   });
+//   newRow.firstElementChild.nextElementSibling.firstElementChild.addEventListener(
+//     "click",
+//     (e) => {
+//       editTask(e, false);
+//     }
+//   );
+//   newRow.lastElementChild.firstElementChild.addEventListener("click", (e) => {
+//     removeRow(e, false);
+//   });
 //   return newRow;
 // };
