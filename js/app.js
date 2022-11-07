abfabe3e87430e4d6130aea5b9eb46a2fb0bf590
davidcalero <calerodonaire@gmail.com>
2022-11-07 20:48:13 +0100

    review with antonio repository

---
+++ js/app.js
@@ -0,0 +1,154 @@
+window.addEventListener("load", () => {
+    // Select modal elements
+    let addedToCart = new bootstrap.Modal(
+        document.querySelector("#addedToCart"),
+        {
+            keyboard: false,
+        }
+    );
+
+    let cartPurchased = new bootstrap.Modal(
+        document.querySelector("#cartPurchased"),
+        {
+            keyboard: false,
+        }
+    );
+
+    let alreadyAddedToCart = new bootstrap.Modal(
+        document.querySelector("#alreadyAddedToCart"),
+        {
+            keyboard: false,
+        }
+    );
+    // Add product to cart with buttons
+    let addToCartButtons = document.querySelectorAll(".card-body");
+    for (let index = 0; index < addToCartButtons.length; index++) {
+        let button = addToCartButtons[index];
+        button.addEventListener("click", (e) => {
+        addToCartClicked(e)
+        });
+    }
+    // Emptying cart
+    document.querySelector("#empty-cart").addEventListener("click", () => {
+        emptyCart();
+    });
+    // Simulating payment confirmation
+    document.querySelector("#btn-purchased").addEventListener("click", () => {
+        // purchaseClicked();
+    });
+    // Stop pulse effect
+    document.querySelector("#cart").addEventListener("click", () => {
+        cart.classList.remove("hvr-pulse");
+    });
+    // Activate/Deactivate cart buttons
+    const toggleShoppingControls = (enable) => {
+        if (enable) {
+            document.querySelector("#empty-cart").classList.remove("disabled")
+            document.querySelector("#btn-purchase").classList.remove("disabled")
+            document.querySelector("#btn-purchase").classList.add("hvr-pulse")
+        } else {
+            document.querySelector("#empty-cart").classList.add("disabled")
+            document.querySelector("#btn-purchase").classList.add("disabled")
+            document.querySelector("#btn-purchase").classList.remove("hvr-pulse")
+        }
+    }
+    // Empty cart
+    const emptyCart = () => {
+        document.querySelector("tbody").innerHTML = "";
+        // updateCartTotal();
+        toggleShoppingControls(false);
+    }
+    // Simulate purchase and show alert
+    const purchaseClicked = () => {
+        document.querySelector("tbody").innerHTML = "";
+        // updateCartTotal();
+        toggleShoppingControls(false);
+        cartPurchased.show();
+        setTimeout(() => {
+            cartPurchased.hide();
+        }, 2000);
+    }
+    // Remove items from cart
+    const removeCartItem = (e) => {
+        let buttonClicked = e.target;
+        let newRow = buttonClicked.parentNode.parentNode.getAttribute("row-number");
+        document.querySelector(`#${newRow}`).remove();
+        let numItemsAdded = document.querySelector("tbody").childElementCount;
+        if (numItemsAdded == 0) {
+            toggleShoppingControls(false);
+        }
+        // updateCartTotal();
+    }
+    // Add item to cart after clicking cart button
+    const addToCartClicked = (e) => {
+        let buttonClicked = e.target;
+        let shopItem = buttonClicked.parentNode.parentNode.parentNode;
+        let productId = shopItem.getAttribute("product-id");
+        let title = shopItem.firstElementChild.getAttribute("alt");
+        let productName = shopItem.lastElementChild.firstElementChild.textContent;
+        let price = shopItem.children[1].firstElementChild.innerText.replace("€ / Kg", "");
+        let imageSrc = shopItem.firstElementChild.src;
+        addItemToCart(productId, title, productName, price, imageSrc);
+        // updateCartTotal();
+    }
+    // Generate row for cart
+    const addItemToCart = (productId, title, productName, price, imageSrc) => {
+        let rowId = "row-number-" + productId;
+        let cartItems = document.querySelectorAll("tr");
+        for (let index = 0; index < cartItems.length; index++) {
+            if (cartItems[index].getAttribute("id") == rowId) {
+                alreadyAddedToCart.show();
+                setTimeout(() => {
+                    alreadyAddedToCart.hide();
+                }, 2500);
+                return;
+            }
+        }        
+        let cartRow = document.createElement("tr");
+        cartRow.setAttribute("id", rowId);
+        let itemsAdded = cartItems.length;
+        let cartRowContents = `<th scope="row">${itemsAdded}</th>
+        <td>
+           <div class="card border-success mb-3 text-center">
+              <div class="card-body text-success">
+                 <img src="${imageSrc}" alt="${title}" class="img-thumbnail">
+              </div>
+              <div class="card-footer bg-transparent border-success">${productName}</div>
+           </div>
+        </td>
+        <td>
+           <h4 class="card-title pricing-card-title text-center">${price} € <small class="text-muted">/ Kg</small></h4>
+        </td>
+        <td>
+           <div class="row">
+              <div class="d-flex justify-content-center quantity col-12 col-md-6">
+                 <input class="form-control rounded-sm text-dark border-info" type="number" min="1" max="10"
+                    step="1" value="1" row-number="${rowId}">
+                 <div class="quantity-nav">
+                    <div class="quantity-button quantity-up border-info">
+                      <i class="fa-solid fa-circle-arrow-up text-warning"></i>
+                    </div>
+                    <div class="quantity-button quantity-down border-info">
+                      <i class="fa-solid fa-circle-arrow-down text-warning"></i>
+                    </div>
+                 </div>
+              </div>
+              <div class="text-center col-12 col-md-6">
+                 <button type="button" class="btn btn-link text-danger mt-2" row-number="${rowId}">
+                    <span>
+                    <i class="fa-solid fa-circle-xmark"></i>
+                    </span>
+                 </button>
+              </div>
+           </div>
+        </td>`;
+        cartRow.innerHTML = cartRowContents;
+        document.querySelector("tbody").appendChild(cartRow);
+        cartRow.lastElementChild.lastElementChild.lastElementChild.firstElementChild.addEventListener("click", removeCartItem);
+        cartRow.lastElementChild.firstElementChild.firstElementChild.firstElementChild.addEventListener("change", updateCartTotal());
+        cartRow.lastElementChild.firstElementChild.firstElementChild.firstElementChild.addEventListener("input", updateCartTotal());
+        
+    }
+
+// addEventListener closing symbol
+});
--- js/core.js
+++ js/core.js
@@ -1,8 +1,8 @@
 const GenerateContent = class {
-  //Properties
+  //   Propiedades
   fruits = {
-    1: ["01", "grapes", "uvas", 4.95],
-    2: ["02", "kiwis", "kiwis", 3.85],
+    1: ["01", "Grapes", "Uvas", 4.95],
+    2: ["02", "Kiwis", "Kiwis", 3.85],
     3: ["03", "grapefruits", "pomelos", 2.25],
     4: ["04", "cherries", "cerezas", 3.39],
     5: ["05", "blackberries", "moras", 7.87],
@@ -24,67 +24,62 @@ const GenerateContent = class {
     21: ["21", "watermelons", "sandías", 0.49],
   };
 
-  //Methods
+  //   Métodos
   getYear = () => {
     let year = new Date();
-    document.querySelector(".date").innerHTML += year.getFullYear();
+    document.body.querySelector(".date").innerHTML += year.getFullYear();
   };
   getProductFormat = () => {
     let product = `
-      <!-- Product card -->
-          <div
-            class="draggable card mb-4 shadow-sm"
-            draggable="true"
-            product-id="xxx"
-          >
-            <img
-              src="img/fr-nnn.jpg"
-              class="card-img-top"
-              alt="fruitName"
-              draggable="false"
-            />
-            <div class="card-body">
-              <h1 class="card-title pricing-card-title">
-                yyy € <small class="text-muted">/ Kg</small>
-              </h1>
-  
-              <div class="d-grid gap-2">
-                <button type="button" class="btn btn-lg btn-block btn-warning">
-                  Add to cart
-                </button>
-              </div>
-            </div>
-            <div class="card-footer bg-info text-white">
-              <h4 class="my-0 font-weight-normal">zzz</h4>
+    <div
+          class="draggable card mb-4 shadow-sm"
+          draggable="true"
+          product-id="xxx"
+        >
+          <img
+            src="img/fr-nnn.jpg"
+            class="card-img-top"
+            alt="fruitName"
+            draggable="false"
+          />
+          <div class="card-body">
+            <h1 class="card-title pricing-card-title">
+              yyy € <small class="text-muted">/ Kg</small>
+            </h1>
+
+            <div class="d-grid gap-2">
+              <button type="button" class="btn btn-lg btn-block btn-warning">
+                Add to cart
+              </button>
             </div>
           </div>
-      `;
+          <div class="card-footer bg-info text-white">
+            <h4 class="my-0 font-weight-normal">zzz</h4>
+          </div>
+        </div>
+    `;
     return product;
   };
-
   htmlToElement = (html) => {
     let card = document.createElement("template");
     html = html.trim();
     card.innerHTML = html;
     return card.content.firstElementChild;
   };
-
   setPageContent = (fruits) => {
     let card = "";
-    let productContainer = document.querySelector("#products-list");
-    console.log(productContainer);
-    for (let productNumber in fruits) {
+    let productContainer = document.body.querySelector("#products-list");
+    for (const productNumber in fruits) {
       card = this.getProductFormat();
       card = card.replace(/xxx/, fruits[productNumber][0]);
       card = card.replace(/nnn/, fruits[productNumber][0]);
       card = card.replace(/fruitName/, fruits[productNumber][1]);
-      card = card.replace(/yyy/, fruits[productNumber][3]);
       card = card.replace(/zzz/, fruits[productNumber][1].trim());
+      card = card.replace(/yyy/, fruits[productNumber][3]);
       productContainer.appendChild(this.htmlToElement(card));
     }
   };
 };
-
-let content = new GenerateContent();
-content.getYear();
-content.setPageContent(content.fruits);
+let obj = new GenerateContent();
+obj.getYear();
+obj.setPageContent(obj.fruits);
